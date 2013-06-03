f292e6de516a1d6736fbf659a487f41a61766f2b
kimu_shu <alfvegardrisc@gmail.com>
2013-06-03 22:28:45 +0900

    Fix member data type (to support cross compiler on 64-bit environment)

--- include/mruby/vm_nios2.h
+++ include/mruby/vm_nios2.h
@@ -22,63 +22,74 @@ typedef struct mrb_pair {
   mrb_value second;
 } mrb_pair;
 
+#ifdef MRB_MACHINE_NIOS2
+# define VMCTX_VAR32(t, n)    t n
+# define VMCTX_FUNC(r, n, p)  r (*n) p
+#else
+# define VMCTX_VAR32(t, n)    uint32_t n
+# define VMCTX_FUNC(r, n, p)  uint32_t n
+#endif
+
 typedef struct mrb_vm_context {
-  mrb_state *mrb;
+  VMCTX_VAR32(mrb_state *, mrb);
 
-  mrb_pair  (*argary)         (uint32_t bx);
-  void      (*ary_cat)        (mrb_value recv, mrb_value other);
-  mrb_value (*ary_fetch)      (mrb_value recv, uint32_t index);
-  mrb_value (*ary_new)        (mrb_value *array, uint32_t max);
-  void      (*ary_post)       (mrb_value *array, uint32_t pre, uint32_t post);
-  void      (*ary_push)       (mrb_value recv, mrb_value value);
-  void      (*ary_store)      (mrb_value recv, uint32_t index, mrb_value value);
-  mrb_value (*blk_push)       (uint32_t bx);
-  mrb_value (*blockexec)      (mrb_value recv, uint32_t bx);
-  mrb_value (*call)           (void);
-  void      (*ensure_pop)     (uint32_t times);
-  void      (*ensure_push)    (uint32_t bx);
-  void      (*enter)          (uint32_t ax);
-  mrb_value (*getmcnst)       (mrb_value recv, uint32_t sym);
-  void      (*setmcnst)       (mrb_value recv, uint32_t sym, mrb_value value);
-  mrb_value (*getupvar)       (uint32_t b, uint32_t c);
-  void      (*setupvar)       (uint32_t b, uint32_t c, mrb_value value);
-  mrb_value (*hash_fetch)     (mrb_value hash, uint32_t sym);
-  mrb_value (*hash_new)       (mrb_value *array, uint32_t pairs);
-  void      (*hash_store)     (mrb_value hash, uint32_t sym, mrb_value value);
-  mrb_value (*lambda)         (uint32_t bz, uint32_t cm);
-  mrb_value (*load_lit)       (uint32_t bx);
-  uint32_t  (*load_sym)       (uint32_t bx);
-  mrb_value (*newclass)       (mrb_value base, uint32_t sym, mrb_value super);
-  void      (*newmethod)      (mrb_value base, uint32_t sym, mrb_value closure);
-  mrb_value (*newmodule)      (mrb_value base, uint32_t sym);
-  void      (*raise)          (mrb_value obj);
-  mrb_value (*range_new)      (mrb_value first, mrb_value second, uint32_t exclude);
-  mrb_value (*rescue)         (void);
-  void      (*rescue_pop)     (uint32_t times);
-  void      (*rescue_push)    (mrb_code *base, int32_t offset);
-  void      (*ret_break)      (mrb_value obj);
-  void      (*ret_normal)     (mrb_value obj);
-  void      (*ret_return)     (mrb_value obj);
-  mrb_value (*send_array)     (mrb_value *argv, uint32_t sym);
-  mrb_value (*send_normal)    (mrb_value *argv, uint32_t sym, uint32_t argc);
-  mrb_value (*singleton_class)(mrb_value base);
-  void      (*str_cat)        (mrb_value recv, mrb_value other);
-  mrb_value (*str_dup)        (uint32_t bx);
-  mrb_value (*super)          (mrb_value *argv2, uint32_t argc);
-  mrb_value (*target_class)   (void);
-  void      (*stop_vm)        (void);
-  void      (*raise_err)      (mrb_value obj);
-  mrb_value object_class;
-  mrb_value gv;
-  mrb_value cv;
-  mrb_value iv;
-  mrb_value cnst;
+  VMCTX_FUNC(mrb_pair,  argary,         (uint32_t bx));
+  VMCTX_FUNC(void,      ary_cat,        (mrb_value recv, mrb_value other));
+  VMCTX_FUNC(mrb_value, ary_fetch,      (mrb_value recv, uint32_t index));
+  VMCTX_FUNC(mrb_value, ary_new,        (mrb_value *array, uint32_t max));
+  VMCTX_FUNC(void,      ary_post,       (mrb_value *array, uint32_t pre, uint32_t post));
+  VMCTX_FUNC(void,      ary_push,       (mrb_value recv, mrb_value value));
+  VMCTX_FUNC(void,      ary_store,      (mrb_value recv, uint32_t index, mrb_value value));
+  VMCTX_FUNC(mrb_value, blk_push,       (uint32_t bx));
+  VMCTX_FUNC(mrb_value, blockexec,      (mrb_value recv, uint32_t bx));
+  VMCTX_FUNC(mrb_value, call,           (void));
+  VMCTX_FUNC(void,      ensure_pop,     (uint32_t times));
+  VMCTX_FUNC(void,      ensure_push,    (uint32_t bx));
+  VMCTX_FUNC(void,      enter,          (uint32_t ax));
+  VMCTX_FUNC(mrb_value, getmcnst,       (mrb_value recv, uint32_t sym));
+  VMCTX_FUNC(void,      setmcnst,       (mrb_value recv, uint32_t sym, mrb_value value));
+  VMCTX_FUNC(mrb_value, getupvar,       (uint32_t b, uint32_t c));
+  VMCTX_FUNC(void,      setupvar,       (uint32_t b, uint32_t c, mrb_value value));
+  VMCTX_FUNC(mrb_value, hash_fetch,     (mrb_value hash, uint32_t sym));
+  VMCTX_FUNC(mrb_value, hash_new,       (mrb_value *array, uint32_t pairs));
+  VMCTX_FUNC(void,      hash_store,     (mrb_value hash, uint32_t sym, mrb_value value));
+  VMCTX_FUNC(mrb_value, lambda,         (uint32_t bz, uint32_t cm));
+  VMCTX_FUNC(mrb_value, load_lit,       (uint32_t bx));
+  VMCTX_FUNC(uint32_t,  load_sym,       (uint32_t bx));
+  VMCTX_FUNC(mrb_value, newclass,       (mrb_value base, uint32_t sym, mrb_value super));
+  VMCTX_FUNC(void,      newmethod,      (mrb_value base, uint32_t sym, mrb_value closure));
+  VMCTX_FUNC(mrb_value, newmodule,      (mrb_value base, uint32_t sym));
+  VMCTX_FUNC(void,      raise,          (mrb_value obj));
+  VMCTX_FUNC(mrb_value, range_new,      (mrb_value first, mrb_value second, uint32_t exclude));
+  VMCTX_FUNC(mrb_value, rescue,         (void));
+  VMCTX_FUNC(void,      rescue_pop,     (uint32_t times));
+  VMCTX_FUNC(void,      rescue_push,    (mrb_code *base, int32_t offset));
+  VMCTX_FUNC(void,      ret_break,      (mrb_value obj));
+  VMCTX_FUNC(void,      ret_normal,     (mrb_value obj));
+  VMCTX_FUNC(void,      ret_return,     (mrb_value obj));
+  VMCTX_FUNC(mrb_value, send_array,     (mrb_value *argv, uint32_t sym));
+  VMCTX_FUNC(mrb_value, send_normal,    (mrb_value *argv, uint32_t sym, uint32_t argc));
+  VMCTX_FUNC(mrb_value, singleton_class,(mrb_value base));
+  VMCTX_FUNC(void,      str_cat,        (mrb_value recv, mrb_value other));
+  VMCTX_FUNC(mrb_value, str_dup,        (uint32_t bx));
+  VMCTX_FUNC(mrb_value, super,          (mrb_value *argv2, uint32_t argc));
+  VMCTX_FUNC(mrb_value, target_class,   (void));
+  VMCTX_FUNC(void,      stop_vm,        (void));
+  VMCTX_FUNC(void,      raise_err,      (mrb_value obj));
+  VMCTX_VAR32(mrb_value, object_class);
+  VMCTX_VAR32(mrb_value, gv);
+  VMCTX_VAR32(mrb_value, cv);
+  VMCTX_VAR32(mrb_value, iv);
+  VMCTX_VAR32(mrb_value, cnst);
 
-  mrb_irep *irep;
-  mrb_sym *syms;
-  mrb_value *pool;
-  int ai;
+  VMCTX_VAR32(mrb_irep *,  irep);
+  VMCTX_VAR32(mrb_sym *,   syms);
+  VMCTX_VAR32(mrb_value *, pool);
+  VMCTX_VAR32(int,         ai);
 } mrb_vm_context;
+
+#undef VMCTX_VAR32
+#undef VMCTX_FUNC
 
 void mrb_vm_context_init(mrb_state *mrb);
 mrb_value mrb_vm_exec(mrb_state *mrb, mrb_code *code);
