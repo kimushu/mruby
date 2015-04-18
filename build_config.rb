048184990fdd691b59c332a655245322e9d4473f
kimu_shu <alfvegardrisc@gmail.com>
2015-04-18 22:50:31 +0900

    Fix build failure for host-debug

--- build_config.rb
+++ build_config.rb
@@ -103,6 +103,7 @@ MRuby::Build.new('host-debug') do |conf|
 
   # Generate mruby debugger command (require mruby-eval)
   conf.gem :core => "mruby-bin-debugger"
+  conf.gem "mrbgems/mruby-eval"
 
   # bintest
   # conf.enable_bintest
